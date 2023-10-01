5661728aa4630864ab39caccf2ee86857fa86624
cadvincula <CHRISTIANADVINCULA@GMAIL.COM>
2023-10-01 23:13:56 +0800

    added beer service

--- src/main/java/guru/springframework/spring6restmvc/controller/BeerController.java
+++ src/main/java/guru/springframework/spring6restmvc/controller/BeerController.java
@@ -4,18 +4,28 @@ import guru.springframework.spring6restmvc.model.Beer;
 import guru.springframework.spring6restmvc.service.BeerService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.UUID;
-
 
 @Slf4j
 @AllArgsConstructor
-@Controller
+@RestController
+@RequestMapping("/api/v1/beers")
 public class BeerController {
     private final BeerService beerService;
 
-    public Beer getBeerById(UUID id) {
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Beer> listBeers() {
+        return beerService.listBeers();
+    }
+
+    @RequestMapping(value = "{beerId}", method = RequestMethod.GET)
+    public Beer getBeerById(@PathVariable("beerId") UUID id) {
         log.debug("getBeerById in Controller: "+id);
         return beerService.getBeerById(id);
     }
