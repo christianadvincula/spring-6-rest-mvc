caef6d7ec5602a71602b4871f99963d495f101d6
cadvincula <CHRISTIANADVINCULA@GMAIL.COM>
2023-10-07 23:28:48 +0800

    added test for customer

--- src/test/java/guru/springframework/spring6restmvc/Spring6RestMvcApplicationTests.java
+++ src/test/java/guru/springframework/spring6restmvc/Spring6RestMvcApplicationTests.java
@@ -1,10 +1,13 @@
 package guru.springframework.spring6restmvc;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import guru.springframework.spring6restmvc.controller.BeerController;
+import guru.springframework.spring6restmvc.controller.CustomerController;
 import guru.springframework.spring6restmvc.model.Beer;
+import guru.springframework.spring6restmvc.model.Customer;
 import guru.springframework.spring6restmvc.service.BeerService;
 import guru.springframework.spring6restmvc.service.BeerServiceImpl;
+import guru.springframework.spring6restmvc.service.CustomerService;
+import guru.springframework.spring6restmvc.service.CustomerServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -17,7 +20,7 @@ import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@WebMvcTest(BeerController.class)
+@WebMvcTest({BeerController.class, CustomerController.class})
 class Spring6RestMvcApplicationTests {
 
     @Autowired
@@ -25,11 +28,25 @@ class Spring6RestMvcApplicationTests {
 
     @MockBean
     BeerService beerService;
-
     BeerServiceImpl beerServiceImpl = new BeerServiceImpl();
 
+    @MockBean
+    CustomerService customerService;
+    CustomerServiceImpl customerServiceImpl = new CustomerServiceImpl();
+
     @Test
-    void contextLoads() throws Exception {
+    void testListBeers() throws Exception {
+        given(beerService.listBeers()).willReturn(beerServiceImpl.listBeers());
+
+        mockMvc.perform(get("/api/v1/beers")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.length()", is(3)));
+    }
+
+    @Test
+    void testGetBeerById() throws Exception {
         Beer testBeer = beerServiceImpl.listBeers().get(0);
 
         given(beerService.getBeerById(testBeer.getId())).willReturn(testBeer);
@@ -40,6 +57,30 @@ class Spring6RestMvcApplicationTests {
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$.id", is(testBeer.getId().toString())))
                 .andExpect(jsonPath("$.beerName", is(testBeer.getBeerName())));
+    }
+
+    @Test
+    void testListCustomers() throws Exception {
+        given(customerService.getCustomers()).willReturn(customerServiceImpl.getCustomers());
+
+        mockMvc.perform(get("/api/v1/customers")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.length()", is(3)));
+    }
+
+    @Test
+    void testGetCustomerById() throws Exception {
+        Customer customer = customerServiceImpl.getCustomers().get(0);
+        given(customerService.getCustomerById(customer.getId())).willReturn(customer);
+
+        mockMvc.perform(get("/api/v1/customers/"+customer.getId())
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id", is(customer.getId().toString())))
+                .andExpect(jsonPath("$.customerName", is(customer.getCustomerName())));
     }
 
 }
